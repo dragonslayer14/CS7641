8705fa8b339336d0e450d9aab726a9a28169d169
Cody Phrampus <cphrampus3@gatech.edu>
2021-03-13 16:18:59 -0600

    get problem 3 configs locked in

--- P2-RO/main.py
+++ P2-RO/main.py
@@ -355,8 +355,8 @@ def run_RHC_3(problem, init_state, **kwargs):
     # run multiple times to get average
     for random_state in random_states:
         start = time.time()
-        _, best_fit, fit_curve, evals = random_hill_climb(problem, random_state=random_state, **kwargs, curve=True,
-                                                   init_state=init_state, fevals=True)
+        _, best_fit, fit_curve, evals = random_hill_climb(problem, random_state=random_state, restarts=39,
+                                                          **kwargs, curve=True, init_state=init_state, fevals=True)
 
         fit_vals.append(best_fit)
         fit_curves.append(fit_curve)
@@ -393,7 +393,8 @@ def run_GA_3(problem, init_state, **kwargs):
     # run multiple times to get average
     for random_state in random_states:
         start = time.time()
-        _, best_fit, fit_curve, evals = genetic_alg(problem, random_state=random_state, **kwargs, curve=True, fevals=True)
+        _, best_fit, fit_curve, evals = genetic_alg(problem, random_state=random_state, pop_size=170, mutation_prob=0.82,
+                                                    **kwargs, curve=True, fevals=True)
 
         fit_vals.append(best_fit)
         fit_curves.append(fit_curve)
@@ -431,7 +432,8 @@ def run_MIMIC_3(problem, init_state, **kwargs):
     for random_state in random_states:
         start = time.time()
 
-        _, best_fit, fit_curve, evals = mimic(problem, random_state=random_state, **kwargs, curve=True, fevals=True)
+        _, best_fit, fit_curve, evals = mimic(problem, random_state=random_state, pop_size=275,
+                                              **kwargs, curve=True, fevals=True)
 
         fit_vals.append(best_fit)
         fit_curves.append(fit_curve)
@@ -472,8 +474,8 @@ def run_SA_3(problem, init_state, **kwargs):
     for random_state in random_states:
         start = time.time()
 
-        _, best_fit, fit_curve, evals = simulated_annealing(problem, random_state=random_state, **kwargs, curve=True,
-                                                   init_state=init_state, fevals=True)
+        _, best_fit, fit_curve, evals = simulated_annealing(problem, random_state=random_state, schedule=GeomDecay(decay=0.94),
+                                                            **kwargs, curve=True, init_state=init_state, fevals=True)
 
         fit_vals.append(best_fit)
         fit_curves.append(fit_curve)
@@ -583,19 +585,19 @@ if __name__ == "__main__":
     # mlrose grid search doesn't seem too helpful, so just coarse MC curve and narrow on areas for tuning, need the curves for report anyway
 
     # problem 1
-    init_state = init_states = [
-        np.random.randint(0, 2, 20),
-        np.random.randint(0, 2, 50),
-        np.random.randint(0, 2, 100),
-        # np.random.randint(0, 2, 100) # mimic takes a long time to run, will need to run overnight
+    init_states = [
+        np.random.randint(0, 8, 8),
+        # np.random.randint(0, 15, 15),
+        np.random.randint(0, 36, 36),
+        np.random.randint(0, 50, 50)
     ]
     # plt.figure()
-    fit_func = MaxKColor([])
+    fit_func = Queens()
 
     # calls for tuning
     # get medium size
     init_state = init_states[1]
-    problem = DiscreteOpt(length=len(init_state), fitness_fn=fit_func)
+    problem = DiscreteOpt(length=len(init_state), fitness_fn=fit_func, maximize=False)
     # problem = MaxKColorGenerator().generate(seed=123, number_of_nodes=len(init_state),
     #                                         max_connections_per_node=4, max_colors=None)
 
@@ -610,12 +612,12 @@ if __name__ == "__main__":
     # for pop_size in range(0, 51):
     #     values.append(pop_size)
     #     print(pop_size)
-    #     fit, _, fevals = run_RHC_2(problem, init_state, restarts=pop_size)
+    #     fit, _, fevals = run_RHC_3(problem, init_state, restarts=pop_size)
     #     fitness.append(fit)
     #     evals.append(fevals)
     #
     # plt.plot(values, fitness)
-    # plt.savefig("charts/RHC2_restarts")
+    # plt.savefig("charts/RHC3_restarts")
 
     # plt.figure()
     # plt.title("SA initial temperature")
@@ -628,13 +630,13 @@ if __name__ == "__main__":
     # for pop_size in range(1, 21):
     #     values.append(pop_size)
     #     print(pop_size)
-    #     fit, _, fevals = run_SA_2(problem, init_state, schedule=GeomDecay(init_temp=pop_size))
+    #     fit, _, fevals = run_SA_3(problem, init_state, schedule=GeomDecay(decay=0.94, init_temp=pop_size))
     #     fitness.append(fit)
     #     evals.append(fevals)
     #
     # plt.plot(values, fitness)
-    # plt.savefig("charts/SA2_init_temp")
-    #
+    # plt.savefig("charts/SA3_init_temp_with_decay")
+
     # plt.figure()
     # plt.title("SA decay")
     # plt.xlabel("decay rate")
@@ -648,12 +650,12 @@ if __name__ == "__main__":
     #     decay = round(decay, 3)
     #     values.append(decay)
     #     print(decay)
-    #     fit, _, fevals = run_SA_2(problem, init_state, schedule=GeomDecay(init_temp=11, decay=decay))
+    #     fit, _, fevals = run_SA_3(problem, init_state, schedule=GeomDecay(decay=decay))
     #     fitness.append(fit)
     #     evals.append(fevals)
     #
     # plt.plot(values, fitness)
-    # plt.savefig("charts/SA2_decay_init_temp")
+    # plt.savefig("charts/SA3_decay")
 
     # plt.figure()
     # plt.title("GA population size")
@@ -666,12 +668,12 @@ if __name__ == "__main__":
     # for pop_size in range(100, 310, 10):
     #     values.append(pop_size)
     #     print(pop_size)
-    #     fit, _, fevals = run_GA_2(problem, init_state, pop_size=pop_size)
+    #     fit, _, fevals = run_GA_3(problem, init_state, pop_size=pop_size)
     #     fitness.append(fit)
     #     evals.append(fevals)
     #
     # plt.plot(values, fitness)
-    # plt.savefig("charts/GA2_pop_size")
+    # plt.savefig("charts/GA3_pop_size")
 
     # plt.figure()
     # plt.title("GA mutation probability")
@@ -687,13 +689,13 @@ if __name__ == "__main__":
     #     decay = round(decay, 3)
     #     values.append(decay)
     #     print(decay)
-    #     fit, _, fevals = run_GA_2(problem, init_state, pop_size=210, mutation_prob=decay)
+    #     fit, _, fevals = run_GA_3(problem, init_state, mutation_prob=decay)
     #     fitness.append(fit)
     #     evals.append(fevals)
     #
     # plt.plot(values, fitness)
-    # plt.savefig("charts/GA2_mutation_prob_pop_210")
-
+    # plt.savefig("charts/GA3_mutation_prob_pop_170")
+    #
     # plt.figure()
     # plt.title("MIMIC population size")
     # plt.xlabel("pop size")
@@ -705,13 +707,13 @@ if __name__ == "__main__":
     # for pop_size in range(280, 410, 10):
     #     values.append(pop_size)
     #     print(pop_size)
-    #     fit, _, fevals = run_MIMIC_2(problem, init_state, pop_size=pop_size)
+    #     fit, _, fevals = run_MIMIC_3(problem, init_state, pop_size=pop_size)
     #     fitness.append(fit)
     #     evals.append(fevals)
     #
     # plt.plot(values, fitness)
-    # plt.savefig("charts/MIMIC2_pop_size_300+")
-
+    # plt.savefig("charts/MIMIC3_pop_size_300+")
+    #
     # plt.figure()
     # plt.title("MIMIC keep percentage")
     # plt.xlabel("keep %")
@@ -725,17 +727,18 @@ if __name__ == "__main__":
     #     decay = round(decay, 3)
     #     values.append(decay)
     #     print(decay)
-    #     fit, _, fevals = run_MIMIC_2(problem, init_state, pop_size=300, keep_pct=decay)
+    #     fit, _, fevals = run_MIMIC_3(problem, init_state, pop_size=275, keep_pct=decay)
     #     fitness.append(fit)
     #     evals.append(fevals)
     #
     # plt.plot(values, fitness)
-    # plt.savefig("charts/MIMIC2_keep_pct_pop_300")
-
-
-
-
-    plt.show()
+    # plt.savefig("charts/MIMIC3_keep_pct_pop_275")
+    #
+    #
+    #
+    #
+    # plt.show()
+    # print()
 
     # run_GA_1(problem, init_state)
     # run_MIMIC_1(problem, init_state)
@@ -853,8 +856,9 @@ if __name__ == "__main__":
 
     init_states = [
         np.random.randint(0, 8, 8),
-        np.random.randint(0, 15, 15),
-        np.random.randint(0, 36, 36)
+        # np.random.randint(0, 15, 15),
+        np.random.randint(0, 36, 36),
+        np.random.randint(0, 50, 50)
     ]
     # plt.figure()
     fit_func = Queens()
@@ -875,11 +879,15 @@ if __name__ == "__main__":
     GA_vals = []
     MIMIC_vals = []
     for init_state in init_states:
-        problem = DiscreteOpt(length=len(init_state), fitness_fn=fit_func, maximize=False, max_val=len(init_state))
-        RHC_vals.append(run_RHC_3(problem, init_state))
-        SA_vals.append(run_SA_3(problem, init_state))
-        GA_vals.append(run_GA_3(problem, init_state))
-        MIMIC_vals.append(run_MIMIC_3(problem, init_state))
+        problem = DiscreteOpt(length=len(init_state), fitness_fn=fit_func, maximize=False)
+        fit, _,_ = run_RHC_3(problem, init_state)
+        RHC_vals.append(fit)
+        fit, _,_ = run_SA_3(problem, init_state)
+        SA_vals.append(fit)
+        fit, _,_ = run_GA_3(problem, init_state)
+        GA_vals.append(fit)
+        fit, _,_ = run_MIMIC_3(problem, init_state)
+        MIMIC_vals.append(fit)
     plt.plot(lens, RHC_vals, label="rhc")
     plt.plot(lens, SA_vals, label="sa")
     plt.plot(lens, GA_vals, label="ga")
@@ -892,7 +900,7 @@ if __name__ == "__main__":
     plt.ylabel("fitness")
     plt.legend()
     plt.savefig(f"charts/{problem_name}")
-    # plt.show()
+    plt.show()
     plt.close('all')
 
     # ANN compare
